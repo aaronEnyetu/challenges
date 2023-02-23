1b576d61fa63b099ef8774eb1e069f60c22d1052
Aaron Enyetu <aaronenyetu@gmail.com>
2023-02-23 15:41:41 -0800

    completed camel case method challenge

---
+++ Camel Case Method/camelCase.js
@@ -0,0 +1,8 @@
+String.prototype.camelCase = function() {
+    let arrWords = this.trim().split(' ');
+    let modifyArrWords = arrWords.map(item => {
+        return item = `${item.charAt(0).toUpperCase()}${item.slice(1)}`;
+    });
+    return modifyArrWords.join('');
+};
+console.log(' hello case Word '.camelCase());
